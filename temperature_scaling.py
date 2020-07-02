d85f64ad289660692a83d0f66bbf5ead55ee8bc0
Mayuran Surendran <mayuran.surendran@tum.de>
2020-07-02 19:34:36 +0200

    Temperature Scaling.

---
+++ temperature_scaling.py
@@ -0,0 +1,253 @@
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from dataprovider import DataGenerator
+from scipy.optimize import minimize
+from sklearn.metrics import log_loss
+from tensorflow.keras.optimizers import Adam
+from utils_temperature_scaling import *
+
+
+class TemperatureScaling():
+    def __init__(self, temp=1.0, maxiter=1, solver="BFGS"):
+        self.temp = temp
+        self.maxiter = maxiter
+        self.solver = solver
+
+    def _loss_fun(self, x, probs, true, mask):
+        classes = np.linspace(0, 64, 64, dtype=int)
+        scaled_probs = self.predict(probs, x, training=True)
+        loss = log_loss(y_true=true, y_pred=scaled_probs, labels=classes, sample_weight=mask)
+
+        return loss
+
+    def fit(self, logits, true, mask):
+        #true = true.flatten()  # Flatten y_val
+        opt = minimize(self._loss_fun, x0=self.temp, args=(logits, true, mask), options={'maxiter': self.maxiter},
+                       method=self.solver)
+        self.temp = opt.x[0]
+
+        return opt
+
+    def predict(self, logits, temp=None, training=True):
+        axis = 1 if training else 3
+        if not temp:
+            return softmax(logits / self.temp, axis=axis)
+        else:
+            return softmax(logits / temp, axis=axis)
+
+
+def fit_TemperatureCalibration(X_val, y_val, mask, T=tf.Variable(tf.ones(shape=(1, ))), epochs=100):
+    history = []
+    optimizer = Adam(learning_rate=1e-3)
+
+    def cost(T, x, y, mask):
+        for i in range(0, x.shape[1]):
+            x[:, i] = tf.multiply(x=x[:, i], y=mask)
+        y = tf.multiply(x=y, y=mask)
+        scaled_logits = tf.multiply(x=x, y=1.0/T)
+
+        cost_value = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(logits=scaled_logits, labels=y)
+        )
+
+        return cost_value
+
+    def grad(T, x, y):
+        with tf.GradientTape() as tape:
+            cost_value = cost(T, x, y, mask)
+
+        return cost_value, tape.gradient(cost_value, T)
+
+    for epoch in range(epochs):
+        train_cost, grads = grad(T, X_val, y_val)
+        optimizer.apply_gradients(zip([grads], [T]))
+        history.append([train_cost, T.numpy()[0]])
+
+    history = np.asarray(history)
+    temperature = history[-1, -1]
+
+    return history, temperature
+
+
+if __name__ == '__main__':
+    train_path = glob.glob("P:/casp7/casp7/training/100/*")
+    val_path = glob.glob("P:/casp7/casp7/validation/1")
+    train_plot = True
+    validation_plot = True
+
+    params = {
+        "crop_size": 64,  # this is the LxL
+        "datasize": None,
+        "features": "pri-evo",  # this will decide the number of channel, with primary 20, pri-evo 41
+        "padding_value": 0,  # value to use for padding the sequences, mask is padded by 0 only
+        "minimum_bin_val": 2,  # starting bin size
+        "maximum_bin_val": 22,  # largest bin size
+        "num_bins": 64,  # num of bins to use
+        "batch_size": 16,  # batch size for training, check if this is needed here or should be done directly in fit?
+        "shuffle": True,  # if wanna shuffle the data, this is not necessary
+        "shuffle_buffer_size": None,  # if shuffle is on size of shuffle buffer, if None then =batch_size
+        "random_crop": True,  # if cropping should be random, this has to be implemented later
+        "flattening": True,
+        # "take":8,
+        "epochs": 30,
+        "prefetch": True,
+        "val_path": val_path,
+        "validation_thinning_threshold": 50,
+        "training_validation_ratio": 0.2,
+        # "experimental_val_take": 2
+    }
+
+    dataprovider = DataGenerator(train_path, **params)
+
+    print("Total Dataset size = {}".format(len(dataprovider)))
+
+    if params.get("val_path", None) is not None:
+        validation_data = dataprovider.get_validation_dataset()
+        validation_steps = dataprovider.get_validation_length()
+        print("Validation Dataset size = {}".format(validation_steps))
+
+    # this is just for experimenting
+    if params.get("experimental_val_take", None) is not None:
+        validation_steps = params.get("experimental_val_take", None)
+        print("Experimenting on validation Dataset size = {}".format(validation_steps))
+
+    # if path is wrong this will throw error
+    if len(dataprovider) <= 0:
+        raise ValueError("Data reading failed!")
+
+    # to find number of steps for one epoch
+    try:
+        num_of_steps = params["take"]
+    except:
+        num_of_steps = len(dataprovider)
+
+    dataprovider = DataGenerator(val_path, **params)
+    if params.get("train_path", None) is not None:
+        validation_data = dataprovider.get_validation_dataset()
+        if params.get("experimental_val_take", None) is not None:
+            validation_steps = params.get("experimental_val_take", None)
+        else:
+            validation_steps = dataprovider.get_validation_length()
+
+    features = 'pri-evo'
+    output_channels = 64
+    num_blocks = [28]
+    num_channels = [64]
+    dilation = [1, 2, 4, 8]
+    batch_size = 16
+    crop_size = 64
+    dropout_rate = 0.1
+    reg_strength = 1e-4
+    weights_path = "P:/proteinfolding_alphafold/minifold_trained/custom_model_weights_epochs_30_batch_size_16"
+
+    model = model_with_logits_output(features=features, model_weights=weights_path, output_channels=output_channels,
+                                     num_blocks=num_blocks, num_channels=num_channels, dilation=dilation,
+                                     batch_size=batch_size, crop_size=crop_size, dropout_rate=dropout_rate,
+                                     reg_strength=reg_strength)
+    temperatures = [1.]
+    ece_before = []
+    ece_after = []
+    if validation_plot:
+        if params.get("val_path", None) is not None:
+            tp = TemperatureScaling()
+            for j, val in enumerate(validation_data):
+                X, ground_truth, mask = val
+                mask = mask.numpy()
+                ground_truth = ground_truth.numpy()
+                output = model.predict(X)
+                confids = softmax(output, axis=3)
+                labels = np.argmax(ground_truth, axis=3)
+                preds = np.argmax(confids, axis=3)
+                confs = np.max(confids, axis=3)
+
+                confs = np.reshape(confs, -1)
+                preds = np.reshape(preds, -1)
+                mask = np.reshape(mask, -1)
+                labels = np.reshape(labels, -1)
+
+                accuracies, confidences, bin_lengths = get_bin_info(confs, preds, labels, mask, bin_size=0.1)
+                ece = expected_calibration_error(confs, preds, labels, mask)
+                ece_before.append(ece)
+                print("ECE: " + str(ece))
+                """
+                print("Before Temperature Scaling:")
+                print("Accuracies: " + str(accuracies))
+                print("Confidences: " + str(confidences))
+                print("ECE: " + str(ece))
+                print("================================")
+
+                
+                X, ground_truth, mask = val
+                mask = mask.numpy()
+                ground_truth = ground_truth.numpy()
+                output = model.predict(X)
+                output = np.reshape(output, (-1, 64))
+                mask = np.reshape(mask, (-1))
+                opt = tp.fit(output, labels, mask)
+                print("Temperature: " + str(tp.temp))
+                """
+
+                X, ground_truth, mask = val
+                mask = mask.numpy()
+                ground_truth = ground_truth.numpy()
+                output = model.predict(X)
+                output = np.reshape(output, (-1, 64))
+                mask = np.reshape(mask, (-1))
+                if j % 5 != 0:
+                    if j == 0:
+                        history, T = fit_TemperatureCalibration(output, labels, mask)
+                    else:
+                        history, T = fit_TemperatureCalibration(output, labels, mask, T=tf.Variable([temperatures[-1]]))
+                    print("Temperature: " + str(T) + ", Loss: " + str(history[-1][0]))
+                    temperatures.append(T)
+                else:
+                    print("Predicting with learned temperature..")
+
+                X, ground_truth, mask = val
+                mask = mask.numpy()
+                ground_truth = ground_truth.numpy()
+                logits = model.predict(X)
+                print("Temperature: " + str(temperatures[-1]))
+                confids_scaled = tp.predict(logits, temp=temperatures[-1], training=False)
+                labels_scaled = np.argmax(ground_truth, axis=3)
+                preds_scaled = np.argmax(confids_scaled, axis=3)
+                confs_scaled = np.max(confids_scaled, axis=3)
+
+                confs_scaled = np.reshape(confs_scaled, -1)
+                preds_scaled = np.reshape(preds_scaled, -1)
+                mask = np.reshape(mask, -1)
+                labels_scaled = np.reshape(labels_scaled, -1)
+
+                accuracies_scaled, confidences_scaled, bin_lengths_scaled = get_bin_info(confs_scaled, preds_scaled,
+                                                                                         labels_scaled, mask,
+                                                                                         bin_size=0.1)
+
+                ece_scaled = expected_calibration_error(confs_scaled, preds_scaled, labels_scaled, mask)
+                ece_after.append(ece_scaled)
+                print("ECE: " + str(ece_scaled))
+                print("================================")
+
+                """
+                print("After Temperature Scaling:")
+                print("Accuracies: " + str(accuracies_scaled))
+                print("Confidences: " + str(confidences_scaled))
+                print("ECE: " + str(ece_scaled))
+                print("================================")
+
+                plt.style.use('ggplot')
+                fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(22.5, 4), sharex='col', sharey='row')
+                rel_diagram_sub(accuracies, confidences, ax)
+                plt.show()
+
+                plt.style.use('ggplot')
+                fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(22.5, 4), sharex='col', sharey='row')
+                rel_diagram_sub(accuracies_scaled, confidences_scaled, ax)
+                plt.show()
+                """
+
+    print(sum(ece_before)/len(ece_before))
+    print(sum(temperatures)/len(temperatures))
+    print(sum(ece_after)/len(ece_after))
---
+++ utils_temperature_scaling.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+from network import ResNet
+
+
+def softmax(x, axis):
+    exps = np.exp(x - np.max(x, axis=axis, keepdims=True))
+    return exps / np.sum(exps, axis=axis, keepdims=True)
+
+
+def model_with_logits_output(features, model_weights=None, output_channels=64, num_blocks=[28], num_channels=[64],
+                             dilation=[1, 2, 4, 8], batch_size=16, crop_size=64, dropout_rate=0.1, reg_strength=1e-4):
+    if features == 'primary':
+        inp_channel = 20
+    elif features == 'pri-evo':
+        inp_channel = 41
+    else:
+        raise ValueError("Wrong feature selection! Choose 'primary' or 'pri-evo'!")
+
+    nn = ResNet(input_channels=inp_channel, output_channels=output_channels, num_blocks=num_blocks,
+                num_channels=num_channels, dilation=dilation, batch_size=batch_size, crop_size=crop_size,
+                dropout_rate=dropout_rate, reg_strength=reg_strength, logits=False)
+    model = nn.model()
+    model.load_weights(model_weights).expect_partial()
+    model.summary()
+
+    return model
+
+
+def get_bin_info(conf, pred, true, mask, bin_size=0.1):
+    upper_bounds = np.arange(bin_size, 1 + bin_size, bin_size)
+
+    accuracies = []
+    confidences = []
+    bin_lengths = []
+
+    for conf_thresh in upper_bounds:
+        acc, avg_conf, len_bin = compute_accuracy_bin(conf_thresh - bin_size, conf_thresh, conf, pred, true, mask)
+        accuracies.append(acc)
+        confidences.append(avg_conf)
+        bin_lengths.append(len_bin)
+
+    return accuracies, confidences, bin_lengths
+
+
+def compute_accuracy_bin(conf_thresh_lower, conf_thresh_upper, conf, pred, true, mask):
+    filtered_tuples = [x for x in zip(pred, true, conf, mask) if conf_thresh_lower < x[2] <= conf_thresh_upper and x[3] == 1]
+
+    if len(filtered_tuples) < 1:
+        return 0, 0, 0
+    else:
+        correct = len([x for x in filtered_tuples if x[0] == x[1]])  # How many correct labels
+        len_bin = len(filtered_tuples)  # How many elements falls into given bin
+        avg_conf = sum([x[2] for x in filtered_tuples]) / len_bin  # Avg confidence of BIN
+        accuracy = float(correct) / len_bin  # accuracy of BIN
+        return accuracy, avg_conf, len_bin
+
+
+def rel_diagram_sub(accs, confs, ax, n_bins=10, name="Reliability Diagram", xname="", yname=""):
+    acc_conf = np.column_stack([accs, confs])
+    acc_conf.sort(axis=1)
+    outputs = acc_conf[:, 0]
+    gap = acc_conf[:, 1]
+
+    bin_size = 1/n_bins
+    positions = np.arange(0+bin_size/2, 1+bin_size/2, bin_size)
+
+    # Plot gap first, so its below everything
+    gap_plt = ax.bar(positions, gap, width=bin_size, edgecolor="red", color="red", alpha=0.3, label="Gap", linewidth=2,
+                     zorder=2)
+
+    # Next add error lines
+    # for i in range(n_bins):
+    #     plt.plot([i/n_bins, 1], [0, (n_bins - i) / n_bins], color="red", alpha=0.5, zorder=1)
+
+    # Bars with outputs
+    output_plt = ax.bar(positions, outputs, width=bin_size, edgecolor="black", color="blue", label="Outputs", zorder=3)
+
+    # Line plot with center line.
+    ax.set_aspect('equal')
+    ax.plot([0, 1], [0, 1], linestyle="--")
+    ax.legend(handles=[gap_plt, output_plt])
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_title(name, fontsize=24)
+    ax.set_xlabel(xname, fontsize=22, color="black")
+    ax.set_ylabel(yname, fontsize=22, color="black")
+
+
+def expected_calibration_error(conf, pred, true, mask, bin_size=0.1):
+    upper_bounds = np.arange(bin_size, 1 + bin_size, bin_size)  # Get bounds of bins
+
+    n = np.count_nonzero(mask)
+    ece = 0  # Starting error
+
+    for conf_thresh in upper_bounds:  # Go through bounds and find accuracies and confidences
+        acc, avg_conf, len_bin = compute_accuracy_bin(conf_thresh - bin_size, conf_thresh, conf, pred, true, mask)
+        ece += np.abs(acc - avg_conf) * len_bin / n  # Add weighted difference to ECE
+
+    return ece
